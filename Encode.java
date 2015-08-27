da41fd9498b966bdb225f528a9b446a2f0a7345e
tincan24 <tincan24@gmail.com>
2015-08-27 16:20:28 -0700

    Create Encode.java

---
+++ Encode.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+import java.io.File;
+import java.awt.image.BufferedImage;
+
+public class Encode {
+
+	public static String readFile(String fileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    try {
+	    	// read each line of input text file
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
+
+	        while (line != null) {
+	            sb.append(line);
+	            sb.append("\n");
+	            line = br.readLine();
+	        }
+	        return sb.toString();
+	    } finally {
+	        br.close();
+	    }
+	}
+	
+	public static void encode(String imgName, String imgPath, String fileName) throws IOException {
+		try{
+			// error checking
+			if(imgName == null || imgPath == null || fileName == null){
+				JOptionPane.showMessageDialog(null, "Invalid file. Try again.");
+				return;
+			}
+			
+			// parse input text
+			String text = readFile(fileName);
+			boolean[] textBits = ASCII.encode(text);
+			
+			// set up images
+			BufferedImage inImg = ImageIO.read(new File(imgPath));
+			int height = inImg.getHeight();
+			int width = inImg.getWidth();
+			BufferedImage outImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			
+			// change each pixel
+			int rgb = 0;
+			int newRGB = 0;
+			int index = 0;
+			for(int h = 0; h < height; h++){
+				for(int w = 0; w < width; w++){
+					// get rgb value of each pixel
+					rgb = inImg.getRGB(w, h);
+					String srgb = Integer.toBinaryString(rgb);
+					
+					// change pixel bit
+					if(index < textBits.length){
+						if(textBits[index])
+							srgb = srgb.substring(0, srgb.length()-1) + "1";
+						else
+							srgb = srgb.substring(0, srgb.length()-1) + "0";
+						index++;
+					}
+					
+					// set rgb value of outImg
+					newRGB = Integer.parseUnsignedInt(srgb, 2);
+					outImg.setRGB(w, h, newRGB);
+				}
+			}
+			
+			// save encoded image 
+			File outputFile = new File(imgName);
+			ImageIO.write(outImg, "png", outputFile);
+		} catch(Exception e){
+			JOptionPane.showMessageDialog(null, e);
+		}
+	}
+}
