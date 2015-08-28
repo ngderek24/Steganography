7054e1a09dc5dbc5cd7b58d2f57d65e9b25bb6da
tincan24 <tincan24@gmail.com>
2015-08-28 16:59:13 -0700

    Update ASCII.java

--- ASCII.java
+++ ASCII.java
@@ -1,44 +1,23 @@
-import java.util.Scanner;
-
 public class ASCII {
-
-	// print an array of booleans as 1s and 0s
-	private static void printBooleanArray(boolean[] bits) {
-	    for (int i = 0; i < bits.length; i++) {
-	        // print a 1 for true or 0 for false
-	        if (bits[i]) System.out.print("1");
-	        else         System.out.print("0");
-	    }
-
-	    System.out.println(); // add a newline
-	}
-	
-	public static void main(String[] args) {
-		// testing encode
-		Scanner reader = new Scanner(System.in);
-		String input = reader.nextLine();
-	    boolean[] bits = encode(input);
-	    printBooleanArray(bits);
-
-	    // testing decode
-	    String s = decode(bits);
-	    System.out.println(s);
-	    reader.close();
-	}
-
+	// given a binary string, return bool array representation with true = 1 and false = 0
 	public static boolean[] encode(String msg){
+		// error checking
 		if(msg == null)
 			return null;
 		
 		int size = msg.length();
-		boolean[] res = new boolean[7*size];
+		boolean[] res = new boolean[7*(size+1)];
 		int resIndex = 0;
 		for(int i = 0; i < size; i++){
+			// get each char and get its binary representation
 			char c = msg.charAt(i);
-			int t = (int)c;
-			String binary = Integer.toBinaryString(t);
-			if(binary.length() < 7)
+			String binary = Integer.toBinaryString((int)c);
+			
+			// length of binary of each char must be 7
+			while(binary.length() < 7)
 				binary = "0" + binary;
+			
+			// set array according to bit of char
 			for(int j = 0; j < binary.length(); j++){
 				if(binary.charAt(j) == '1')
 					res[resIndex] = true;
@@ -47,25 +26,13 @@ public class ASCII {
 				resIndex++;
 			}
 		}
+		
+		// add special char at the end of document
+		for(int k = 0; k < 7; k++){
+			res[resIndex] = true;
+			resIndex++;
+		}
 		return res;
 	}
 	
-	public static String decode(boolean[] bits){
-		if(bits == null)
-			return null;
-		
-		int numChars = bits.length/7;
-		String res = "";
-		for(int i = 0; i < numChars; i++){
-			int binNum = 0;
-			for(int j = 0; j < 7; j++){
-				if(bits[i*7 + j] == true)
-					binNum += 1;
-				if(j != 6)
-					binNum <<= 1;
-			}
-			res += (char)binNum;
-		}	
-		return res;
-	}
 }
